778a0c9a694a0581814c50156e0b4b08b156b160
jhon <jhon@jhon-Q470C-Q468C-500P4C>
2016-04-28 11:30:03 -0500

    Se crean tablas relacionales,se mapean las mismas

--- AppBicicleteriasShops/AppBicicleteriasShops-ejb/src/java/com/co/bicicleterias/shops/scripts/DbBicicleteriasShops.sql
+++ AppBicicleteriasShops/AppBicicleteriasShops-ejb/src/java/com/co/bicicleterias/shops/scripts/DbBicicleteriasShops.sql
@@ -15,4 +15,12 @@ phone numeric (15),
 id serial,
 constraint biker_pk primary key(id));
 
-
+create table biker(
+name varchar(30),
+document numeric(15),
+login varchar(30),
+password varchar(30),
+mail varchar(40),
+phone numeric (15),
+id serial,
+constraint biker_pk primary key(id));
